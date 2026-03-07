de55a09b1700f3a933be2dfd306dda4cb9f3ff0e
CodeSageAbhijit <abhijitkad62@gmail.com>
2026-03-07 16:32:23 +0530

    fixed emailjs error

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -36,11 +36,10 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
-          from_name: form.name,
-          to_name: "Abhijeet Kad",
-          from_email: form.email,
-          to_email: "abhijitkad62@gmail.com",
+          name: form.name,
+          email: form.email,
           message: form.message,
+          title: `Portfolio Contact from ${form.name}`,
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
       )
